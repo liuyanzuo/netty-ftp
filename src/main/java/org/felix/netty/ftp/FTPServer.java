bcba0e349aade3cd84c8bf416e38f7e02d1fdfa8
liuyanzuo <215256714@qq.com>
2018-08-16 01:32:23 +0800

    [功能新增]-[添加FTPServer入口，简单测试日志格式]

---
+++ src/main/java/org/felix/netty/ftp/FTPServer.java
@@ -0,0 +1,19 @@
+package org.felix.netty.ftp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * FTP服务器的启动入口
+ *
+ * @author Felix
+ */
+public class FTPServer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(FTPServer.class);
+
+    public static void main(String[] args) {
+        LOG.info("[FTP Server starting]");
+    }
+
+}
